c022442ca7f1ab94f2e95456d9dcb76c0f45704e
Mattia Cugusi <m.cugusi287gmail.com>
2024-10-19 12:02:54 +0200

    esercizio completato

--- invio.php
+++ invio.php
@@ -12,15 +12,32 @@
     $oggetto = $_POST["oggetto"];
     $corpo = $_POST["corpo"];
 
+    $caratteri = strlen($corpo);
+    $parole = str_word_count($corpo);
     echo "<h1>Mail creata</h1>
-    <div style='background-color:light-blue'>
-      <p>Mittente:$mittente</p>
-      <p>Destinatario:$destinatario</p>
-      <p>Oggetto:$oggetto</p>
+    <h3>Informazioni</h3>
+    
+    <div style='background-color:lightblue; padding: 3px'>
+      <hr>
+      <p>Mittente: " . $mittente . "</p>
+      <hr>
+      <p>Destinatario: " . $destinatario . "</p>
+      <hr>
+      <p>Oggetto: " .$oggetto . "</p>
+      <hr>
       </div>
       <h3>Corpo del messaggio:</h3>
-      <p>$corpo</p>
-    </div>";
+      <div style='background-color: orange; padding: 3px; width: 50%;'>
+      <p>" . $corpo . "</p>
+    </div>
+    <hr>
+    <h3>Statistiche corpo mail</h3>
+    <div style='background-color: grey; padding: 3px;'>
+    <p>Lunghezza corpo e-mail: " . $caratteri . " caratteri; </p>
+    <p>Numero parole contenute nel corpo della mail: "  . $parole . " 
+    </div>
+    ";
+    
     ?>
 </body>
 </html>
